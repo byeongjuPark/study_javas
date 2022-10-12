8cce915de370e40564f06b974cd7712a1fa6d60b
byeongjuPark <bottlepark28@gmail.com>
2022-10-12 16:37:14 +0900

    2525

--- src/KOI.java
+++ src/KOI.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 //refer : https://www.acmicpc.net/problem/2525
-public class Main {
+public class KOI {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int h, m, time;
