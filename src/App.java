79b003c4c5b5072fd3d269b73ad93c805c07a76d
byeongjuPark <bottlepark28@gmail.com>
2022-10-12 16:04:38 +0900

    update

--- src/App.java
+++ src/App.java
@@ -2,12 +2,6 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner in = new Scanner(System.in);
-        int A = in.nextInt();
-        int B = in.nextInt();
-
-        System.out.println(A+B);
-        in.close();
-        //System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
     }
 }
---
+++ src/Quadrant.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+// x좌표와 y좌표가 모두 양수 1
+// x좌표가 음수 y좌표 양수 2
+// x좌표와 y좌표가 모두 음수 3
+// x좌표 양수, y좌표 음수 4
+
+//refer : https://www.acmicpc.net/problem/14681
+public class Quadrant{
+    public static void main(String[] args) {
+        int x, y;
+        Scanner sc = new Scanner(System.in);
+        x = sc.nextInt();
+        y = sc.nextInt();
+
+        if(x>0 && y>0){
+            System.out.print(1);
+        }else if(x<0 && y>0){
+            System.out.print(2);
+        }else if(x<0 && y<0){
+            System.out.print(3);
+        }else if(x>0 && y<0){
+            System.out.print(4);
+        }
+    }
+}
