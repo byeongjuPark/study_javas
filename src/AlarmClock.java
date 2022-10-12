22c297027c9adfbeac155601b3b385cc006483e6
byeongjuPark <bottlepark28@gmail.com>
2022-10-12 16:17:56 +0900

    update

--- src/AlarmClock.java
+++ src/AlarmClock.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
-
-public class Main{
+//refer : https://www.acmicpc.net/problem/2884
+public class AlarmClock{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int h, m;
