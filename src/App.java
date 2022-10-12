c5da2a13d832b1db95f47d65b89a43feff46a6be
byeongjuPark <bottlepark28@gmail.com>
2022-10-12 12:52:13 +0900

    Add Add, Minus

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+
+        System.out.println(A+B);
+        in.close();
+        //System.out.println("Hello, World!");
     }
 }
