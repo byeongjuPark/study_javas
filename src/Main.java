7ce2f471e1cb3083858a36d81882a48d5b395757
byeongjuPark <bottlepark28@gmail.com>
2022-10-13 14:59:32 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,8 @@
-import java.util.Scanner;
-//refer : https://www.acmicpc.net/problem/2525
+
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int h, m, time;
-        h = sc.nextInt();
-        m = sc.nextInt();
-        time = sc.nextInt();
-        m = m+time;
-        while(m>59){
-            m-=60;
-            h++;
-            if(h>23){
-                h=0;
-            }
-        }
-        System.out.println(h + " " + m);
-      
-
+    
+        System.out.println("Hymn To Love");
+       
     }
 }
