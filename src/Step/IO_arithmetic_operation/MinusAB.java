a120509c0b1068d2bce943b0d6d5ec3316c833af
byeongjuPark <bottlepark28@gmail.com>
2022-10-18 02:41:54 +0900

    update

---
+++ src/Step/IO_arithmetic_operation/MinusAB.java
@@ -0,0 +1,18 @@
+package Step.IO_arithmetic_operation;
+import java.util.Scanner;
+
+public class MinusAB {
+    
+	public static void main(String[] args) {
+		//refer : https://www.acmicpc.net/problem/1001
+		Scanner sc = new Scanner(System.in);
+		int a,b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a-b);
+
+        sc.close();
+	}
+		
+}
+
